97a3466a06c6686ee0133eabfa9520d0aeba3d3f
DanuarWiranata <101251244+DanuarWiranata@users.noreply.github.com>
2022-09-19 20:30:11 +0700

    create tables
    create table cpl
    create table cpmk

---
+++ database/migrations/2022_09_19_125003_create__cpl_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cpl', function (Blueprint $table) {
+            $table->bigIncrements('kode_cpl');
+            $table->integer('nilai_cpl');
+            $table->unsignedBigInteger('kode_mk');
+            $table->foreign("kode_mk")->references("kode_mk")->on("mata_kuliah")->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cpl');
+    }
+};
