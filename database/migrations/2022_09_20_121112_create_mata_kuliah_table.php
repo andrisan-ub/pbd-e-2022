f646fc139622ed53254a6c140d8159aea128f984
Erlis Rizqiyani <erlisrizqiyani@student.ub.ac.id>
2022-09-20 19:17:58 +0700

    Create 2022_09_20_121112_create_mata_kuliah_table.php

---
+++ database/migrations/2022_09_20_121112_create_mata_kuliah_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mata_kuliah', function (Blueprint $table) {
+            $table->string('Kode_MK')->primary();
+            $table->string('Nama_MK');
+            $table->smallInteger('SKS');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mata_kuliah');
+    }
+};
