69a08bbfbc7eb5851bc23d5aade1f3af43f4f460
ABExacta <arenabintang@student.ub.ac.id>
2022-09-20 21:23:03 +0700

    create table
    create table : dosen, mata kuliah

---
+++ database/migrations/2022_09_20_142009_create_mata_kuliah_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mata_kuliah', function (Blueprint $table) {
+            $table->id('kode_mk')->primary;
+            $table->char('nama_mk',45);
+            $table->integer('jumlah_sks');
+            $table->integer('tahun_kurikulum');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mata_kuliah');
+    }
+};
