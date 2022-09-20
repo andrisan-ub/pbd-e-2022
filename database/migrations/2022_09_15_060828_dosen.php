4eada58d3d6d6826894b1e4167b85917a5aa8dc1
auhppy <auhappycahya11@gmail.com>
2022-09-20 19:49:43 +0700

    Test

---
+++ database/migrations/2022_09_15_060828_dosen.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosen', function (Blueprint $table) {
+            $table->integer('nip');
+            $table->char('nama_dosen');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosen');
+    }
+};
