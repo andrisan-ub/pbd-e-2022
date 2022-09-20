aa401cd71a4209bd2a04deb00b66db2345bfac51
naylis14 <saadahnailis240@student.ub.ac.id>
2022-09-20 21:58:43 +0700

    create table
    create table : cpl, cpmk, sub_cpmk

---
+++ database/migrations/2022_09_20_145722_create_sub_cpmk_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_cpmk', function (Blueprint $table) {
+            $table->id('id_sub_cpmk')->primary;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_cpmk');
+    }
+};
