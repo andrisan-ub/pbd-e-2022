4db83253dd02cab40c451e08f6a0c6cc266a7771
ghaniatan <ghaniatan@student.ub.ac.id>
2022-09-20 21:49:49 +0700

    create table
    create table : nilai_mk

--- database/migrations/2022_09_20_120158_create_nilai_mk_table.php
+++ database/migrations/2022_09_20_120158_create_nilai_mk_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('nilai_mk', function (Blueprint $table) {
-            $table->id('ID_NILAI');
+            $table->id('ID_NILAI')->primary();
             $table->integer('TOTAL_NILAI');
             $table->varchar('NILAI_LK01', 20);
             $table->integer('NILAI_LK01');
@@ -23,6 +23,11 @@ return new class extends Migration
             $table->integer('NILAI_LK04');
             $table->integer('NILAI_UTS');
             $table->integer('NILAI_UAS');
+
+            $table->foreignId('DOSEN_NID')->nullable();
+            $table->foreign('DOSEN_NID')->references('NID')->on('DOSEN');
+            $table->foreignId('RUBRIK_PENILAIAN_ID_RUBRIK')->nullable();
+            $table->foreign('RUBRIK_PENILAIAN_ID_RUBRIK')->references('ID_RUBRIK')->on('RUBRIK_PENILAIAN');
         });
     }
 
