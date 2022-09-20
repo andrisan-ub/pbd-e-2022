debe9ca84c32ba959177f2a9fb15520d7613cb47
Wardah Zahra <5wardahzahra@gmail.com>
2022-09-20 20:17:59 +0700

    Create 2022_09_20_125827_create_cpmk_has_sub_cpmk_table.php

---
+++ database/migrations/2022_09_20_125827_create_cpmk_has_sub_cpmk_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cpmk_has_sub_cpmk', function (Blueprint $table) {
+            $table->foreign('kode_sub_cpmk') ->references('kode_sub_cpmk') -> on ('sub_cpmk');
+            $table->foreign('kode_cpmk') ->references('kode_cpmk') -> on ('cpmk');
+            
+            $table->primary(['kode_sub_cpmk','kode_cpmk']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cpmk_has_sub_cpmk');
+    }
+};
