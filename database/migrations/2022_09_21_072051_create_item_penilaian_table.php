13aa13853f7c22f3aff5461f5d2a2d89ea5a0cc1
auhppy <auhappycahya11@gmail.com>
2022-09-21 14:40:41 +0700

    Create 2022_09_21_072051_create_item_penilaian_table.php

---
+++ database/migrations/2022_09_21_072051_create_item_penilaian_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_penilaian', function (Blueprint $table) {
+            $table->text('teori');
+            $table->text('praktik');
+            $table->text('kode_indikator');
+            $table->foreign('kode_indikator')->references('kode_indikator')->on('rubrik_penilian');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_penilaian');
+    }
+};
