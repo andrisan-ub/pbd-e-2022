0e58c0fc6c9f4346cd5aef7613d69bfb40dcbe50
betgirs <103440027+betgirs@users.noreply.github.com>
2022-09-21 13:48:13 +0700

    Create 2022_09_21_064241_create_cpl_has_cpmk_table.php

---
+++ database/migrations/2022_09_21_064241_create_cpl_has_cpmk_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cpl_has_cpmk', function (Blueprint $table) {
+            $table->String('kode_cpl')->unsigned();
+            $table->String('kode_cpmk')->unsigned();
+            $table->foreign('kode_cpl')->references('kode_cpl')->on('cpl') ;
+            $table->foreign('kode_cpmk')->references('kode_cpmk')->on('cpmk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cpl_has_cpmk');
+    }
+};
