0c6dcc03c04a0350dbb0b99908e481fa651fcaea
betgirs <103440027+betgirs@users.noreply.github.com>
2022-09-21 15:23:39 +0700

    Create 2022_09_20_000008_create_lembar_kerja_table.php

---
+++ database/migrations/2022_09_20_000008_create_lembar_kerja_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lembar_kerja', function (Blueprint $table) {
+            
+            $table->string('kode_lk')->primary();
+            $table->bigInteger('nim');
+            $table->string('tujuan');
+            $table->string('judul');
+            $table->string('pengerjaan_tugas');
+            $table->string('bentuk_tugas');
+            $table->string('deskripsi');
+            $table->string('bentuk_format_luaran');
+            $table->string ('waktu_pengerjaan');
+            $table->string('metode');
+            $table->double('bobot');
+            $table->foreign('nim')->references('nim')->on('mahasiswa');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lembar_kerja');
+    }
+};
