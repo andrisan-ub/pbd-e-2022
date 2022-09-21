364b21c5e6d548911c45e33d6e2513b02d6fd9e8
Alifia Putri Budiyanti <101036030+alifiaputri14@users.noreply.github.com>
2022-09-21 14:15:03 +0700

    Table
     mengirim table cpl,indikator, lembar kerja

---
+++ database/migrations/2022_09_19_213131_indikator.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('indikator', function (Blueprint $table) {
+            $table->string('KODE_INDIKATOR')->primary();
+            $table->string('DESKRIPSI_INDIKATOR');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('indikator');
+    }
+};
