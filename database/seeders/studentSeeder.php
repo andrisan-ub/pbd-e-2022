a70f43aa35e82cbed918fdfa046d0ab9283f88fc
mireltambing <103355827+mireltambing@users.noreply.github.com>
2022-09-21 16:09:29 +0700

    Create studentSeeder.php

---
+++ database/seeders/studentSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class studentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('student')->insert([
+            'id' => 1314,
+            'student_id' => 'ini id',
+                    ]);
+    }
+}
