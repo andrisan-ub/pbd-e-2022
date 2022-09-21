dbd6156c67b7cafa8b60a844f48e2f221fbe2fd9
Wardah Zahra <5wardahzahra@gmail.com>
2022-09-21 13:01:36 +0700

    Update 2022_09_20_125827_create_cpmk_has_sub_cpmk_table.php

--- database/migrations/2022_09_20_125827_create_cpmk_has_sub_cpmk_table.php
+++ database/migrations/2022_09_20_125827_create_cpmk_has_sub_cpmk_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('cpmk_has_sub_cpmk', function (Blueprint $table) {
-            $table->bigInteger('kode_sub_cpmk')->unsigned();
-            $table->bigInteger('kode_cpmk')->unsigned();
+            $table->string('kode_sub_cpmk')->unsigned();
+            $table->string('kode_cpmk')->unsigned();
 
             $table->foreign('kode_sub_cpmk') ->references('kode_sub_cpmk') -> on ('sub_cpmk');
             $table->foreign('kode_cpmk') ->references('kode_cpmk') -> on ('cpmk');
