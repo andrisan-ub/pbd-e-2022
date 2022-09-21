ea7623bed96530e6933155dc3b69f87c454a744c
Ahmad Fathoni <99490295+Fathoni243@users.noreply.github.com>
2022-09-21 15:56:55 +0700

    create seeder study_program
    create seeder study_program

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $faker = Faker::create();
+
+        $id_study_program = [random_int(1, 10), random_int(1, 10), random_int(1, 10)];
+        $id_faculty = [random_int(1, 10), random_int(1, 10), random_int(1, 10)];
+        $name = [$faker->text(10), $faker->text(10), $faker->text(10)];
+
+        for ($i=0; $i < sizeof($id_study_program); $i++) { 
+            DB::table('study_program')->insert([
+                'id' => $id_study_program[$i],
+                'faculty_id' => $id_faculty[$i],
+                'name' => $name[$i],
+            ]);
+        }
+
+
     }
 }
