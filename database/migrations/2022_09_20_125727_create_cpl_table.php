6ddba40d39122c2949655eb86eddd211ce1b1319
Wardah Zahra <5wardahzahra@gmail.com>
2022-09-21 11:16:22 +0700

    Update 2022_09_20_125727_create_cpl_table.php

--- database/migrations/2022_09_20_125727_create_cpl_table.php
+++ database/migrations/2022_09_20_125727_create_cpl_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('cpl', function (Blueprint $table) {
             $table->id('kode_cpl');
+            $table->bigInteger('kode_matkul')->unsigned();
             $table->string('materi');
-            $table->foreign('kode_matkul') ->references('kode_matkul') -> on ('matkul');
+            $table->foreign('kode_matkul') ->references('kode_matkul') -> on ('mata_kuliah');
             
-            $table->timestamp;
+            $table->timestamps();
         });
     }
 
