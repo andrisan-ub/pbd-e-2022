326efa01384bddba962726561504f93d0fe80392
Erlis Rizqiyani <erlisrizqiyani@student.ub.ac.id>
2022-09-21 16:06:23 +0700

    Create study_programSeeder.php

---
+++ database/seeders/study_programSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class study_programSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('study_program')->insert([
+            'id' => 567 ,
+            'faculty_id' => 432,
+            'name' => "Teknik Informatika",
+        ]);
+    }
+}
