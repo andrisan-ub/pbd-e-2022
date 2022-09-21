601c6fc167a8e0b95b7f29d63b29e849d207fd17
ghaniatan <ghaniatan@student.ub.ac.id>
2022-09-21 15:56:16 +0700

    Create 2022_09_21_084750_create_users_table.php

---
+++ database/migrations/2022_09_21_084750_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->varchar('email');
+            $table->timestamps('email_verified_at');
+            $table->varchar('name', 255);
+            $table->varchar('name', 100);
+            $table->timestamps('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
