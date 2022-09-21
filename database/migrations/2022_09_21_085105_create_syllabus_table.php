4edfdcf109beb89959139d49b0aa96c01101b6ca
Siti Waheeda Najihah <sitiwaheeda14@student.ub.ac.id>
2022-09-21 15:59:28 +0700

    Create 2022_09_21_085105_create_syllabus_table.php

---
+++ database/migrations/2022_09_21_085105_create_syllabus_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mata_kuliah', function (Blueprint $table) {
+            $table->string('kode_matkul')->primary();
+            $table->bigInteger('dosen_nip');
+            $table->foreign('dosen_nip')->references('nip')->on('dosen');
+            $table->string('nama_matkul');
+            $table->string('program_studi');
+            $table->string('jurusan');
+            $table->string('semester');
+            $table->string('sks');
+            $table->string('sifat');
+            $table->string('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('syllabus');
+    }
+};
