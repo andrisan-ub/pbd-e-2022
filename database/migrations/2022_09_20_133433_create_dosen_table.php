ae948aa39de8f2b084a4b768fab59b0571d845d5
auhppy <auhappycahya11@gmail.com>
2022-09-21 11:23:48 +0700

    Update 2022_09_20_133433_create_dosen_table.php

--- database/migrations/2022_09_20_133433_create_dosen_table.php
+++ database/migrations/2022_09_20_133433_create_dosen_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('dosen', function (Blueprint $table) {
-            $table->id('nip');
+            $table->bigInteger('nip')->primary();
             $table->string('nama_dosen');
+            $table->timestamps();
         });
     }
 
