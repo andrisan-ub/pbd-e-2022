d6af432d3594eb8ccb6d70809bd9916e7a9746de
Ahmad Fathoni <99490295+Fathoni243@users.noreply.github.com>
2022-09-26 12:01:47 +0700

    Revisi
    delete isi DatabaseSeeder.php and create StudyProgramSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,18 +20,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        $id_study_program = [1, 2];
-        $id_faculty = [1, 2];
-        $name = ["Teknologi Informasi", "Teknik Informatika"];
-
-        for ($i=0; $i < sizeof($id_study_program); $i++) { 
-            DB::table('study_program')->insert([
-                'id' => $id_study_program[$i],
-                'faculty_id' => $id_faculty[$i],
-                'name' => $name[$i],
-            ]);
-        }
 
     }
 }
