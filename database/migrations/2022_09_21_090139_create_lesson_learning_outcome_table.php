62bc157b4b4452477c1f48f126b5f5de0781162c
shalsasalisa <shalsasalisa@student.ub.ac.id>
2022-09-27 14:09:24 +0700

    create table
    create table lesson_learning_outcome

---
+++ database/migrations/2022_09_21_090139_create_lesson_learning_outcome_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_lesson_learning_outcome', function (Blueprint $table) {
+            $table->id();
+            $table->integer('clo_id');
+            $table_>foreign('clo_id')->references('id')->on('course_learning_outcome');
+            $table->integer('position')->nullable();
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_lesson_learning_outcome');
+    }
+};
