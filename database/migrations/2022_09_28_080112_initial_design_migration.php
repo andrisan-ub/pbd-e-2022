eea96a58d617650f4c805c6be4c1019aee76b7a2
auhppy <auhappycahya11@gmail.com>
2022-09-28 15:54:19 +0700

    Update 2022_09_28_080112_initial_design_migration.php

--- database/migrations/2022_09_28_080112_initial_design_migration.php
+++ database/migrations/2022_09_28_080112_initial_design_migration.php
@@ -13,9 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('study_program', function (Blueprint $table) {
+        Schema::create('rubric', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('faculty_id');
+            $table->string('name')->nullable();
+            $table->bigInteger('assignment_id')->unsigned();
+            $table->integer('created_at');
+            $table->integer('updated_at')->nullable();
+
+            $table->timestamps();
         });
     }
 
@@ -26,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('study_program');
+        Schema::dropIfExists('rubric');
     }
 };
