00bce4d2000df80bf98cb7a0bfcbb165667a9653
Firdausi Baiq Ardana <firdaardana@student.ub.ac.id>
2022-09-28 15:35:02 +0700

    Update 2022_09_28_080112_initial_design_migration.php

--- database/migrations/2022_09_28_080112_initial_design_migration.php
+++ database/migrations/2022_09_28_080112_initial_design_migration.php
@@ -13,7 +13,24 @@ return new class extends Migration
      */
     public function up()
     {
-
+        Schema::create('assignment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('llo_id');
+            $table->foreign('llo_id')->references('id')->on('lesson_learning_outcome');
+            $table->text('objective');
+            $table->string('title',(2048));
+            $table->boolean('is_group_assignment');
+            $table->string('assignment_style', (1024));
+            $table->text('description');
+            $table->text('output_instruction');
+            $table->text('submission_instruction');
+            $table->text('deadline_instruction');
+            $table->integer('class_id');
+            $table->foreign('class_id')->references('id')->on('class');
+            $table->integer('created_at');
+            $table->integer('updated_at');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +40,6 @@ return new class extends Migration
      */
     public function down()
     {
-
+        Schema::dropIfExists('assignment');
     }
 };
