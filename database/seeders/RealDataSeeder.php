0c714b7daef4ad7de8672c3d7a47158e98d5bbc1
MIFZ <94106059+MIFZywan@users.noreply.github.com>
2022-09-28 15:46:03 +0700

    Update RealDataSeeder.php

--- database/seeders/RealDataSeeder.php
+++ database/seeders/RealDataSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RealDataSeeder extends Seeder
 {
@@ -14,6 +15,24 @@ class RealDataSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('student')->insert([
+            'id' => '1111',
+            'student_id' => '205150701011024'
+        ]);
+
+        DB::table('student')->insert([
+            'id' => '2222',
+            'student_id' => '205150701011025'
+        ]);
+
+        DB::table('student')->insert([
+            'id' => '3333',
+            'student_id' => '205150701011026'
+        ]);
+
+        DB::table('student')->insert([
+            'id' => '4444',
+            'student_id' => '205150701011027'
+        ]);
     }
 }
