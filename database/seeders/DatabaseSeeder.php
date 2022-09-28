cd707a472ddf638d88b4cbb83d7448fbe3e818aa
auvalmarom <auvalmarom253@student.ub.ac.id>
2022-09-28 10:12:09 +0700

    Seeder table faculty

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $faker = Faker::create();
+
+        $id_faculty = [1,2,3];
+        $name = ['Fakultas Ilmu komputer', 'Fakultas Kedokteran', 'Fakultas Teknik'];
+
+        for($i=0; $i<sizeof($id_faculty); $i++){
+            DB::table('faculty')->insert([
+                'id' => $id_faculty[$i],
+                'name' => $name[$i]
+            ])
+        }
     }
 }
