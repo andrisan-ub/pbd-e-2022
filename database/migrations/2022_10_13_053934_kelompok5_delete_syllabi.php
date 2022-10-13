75814029afc25ece7ad849ec6587001b2b3fc037
nathaliaclarissa <90769498+nathaliaclarissa@users.noreply.github.com>
2022-10-13 13:12:19 +0700

    Create 2022_10_13_053934_kelompok5_delete_syllabi.php

---
+++ database/migrations/2022_10_13_053934_kelompok5_delete_syllabi.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $procedure = "DROP PROCEDURE IF EXISTS `kelompok5_deleteSyllabi`;
+        CREATE PROCEDURE `kelompok5_deleteSyllabi` (IN SyllabiId INT)
+        BEGIN
+            DELETE FROM syllabi
+            WHERE id = SyllabiId;
+        END;";
+
+        \DB::unprepared($procedure);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
