c95f715adac01199fea971fe4d262fad4ef8083f
Wardah Zahra <5wardahzahra@gmail.com>
2022-12-06 21:48:41 +0700

    Update 2022_12_06_131254_create_trigger_insert_schedules.php

--- database/migrations/2022_12_06_131254_create_trigger_insert_schedules.php
+++ database/migrations/2022_12_06_131254_create_trigger_insert_schedules.php
@@ -15,31 +15,50 @@ return new class extends Migration
     public function up()
     {
         DB::unprepared('
-        CREATE TRIGGER `aaa` BEFORE INSERT ON `class_schedules`
+        CREATE TRIGGER `before_insert_schedules` BEFORE INSERT ON `class_schedules`
         FOR EACH ROW BEGIN
-            DECLARE errorMessage VARCHAR(255);
-            DECLARE room VARCHAR(255);
-            DECLARE day VARCHAR(20);
-            DECLARE stime INT;
-            DECLARE etime INT;
-            DECLARE suma INT;
-
-            SET errorMessage = `test`;
-            SET room = (SELECT rooms.name FROM rooms WHERE id = NEW.room_id);
-            SET stime = (SELECT start_times.time FROM start_times WHERE id = NEW.start_id);
-            SET etime = (SELECT end_times.time WHERE id = NEW.end_id);
-            
-            IF 0 < (SELECT COUNT(id) FROM class_schedules
-                    WHERE room_id = NEW.rood.id AND 
-                    day_id = NEW.day_id AND (
-                    start_id = NEW.start.id OR
-                    end_id = NEW.end.id))
-            THEN
-                SIGNAL SQLSTATE \'45000\'
-                SET MESSAGE_TEXT = errorMessage;
-            END IF;
-            
-        END
+                    DECLARE errorMessage VARCHAR(255);
+                    DECLARE room VARCHAR(255);
+                    DECLARE day VARCHAR(20);
+                    DECLARE stime INT;
+                    DECLARE etime INT;
+                    DECLARE suma INT;
+        
+                    SET errorMessage = CONCAT(\'Ruangan sedang dipakai kelas lain\');
+                    
+                    IF 0 < (SELECT COUNT(id) FROM class_schedules
+                            WHERE room_id = NEW.room_id AND 
+                            day_id = NEW.day_id AND (
+                            start_id = NEW.start_id OR
+                            end_id = NEW.end_id))
+                    THEN
+                        SIGNAL SQLSTATE \'45000\'
+                        SET MESSAGE_TEXT = errorMessage;
+                    END IF;
+                    
+                END;
+        CREATE TRIGGER `before_update_schedules` BEFORE INSERT ON `class_schedules`
+        FOR EACH ROW BEGIN
+                    DECLARE errorMessage VARCHAR(255);
+                    DECLARE room VARCHAR(255);
+                    DECLARE day VARCHAR(20);
+                    DECLARE stime INT;
+                    DECLARE etime INT;
+                    DECLARE suma INT;
+        
+                    SET errorMessage = CONCAT(\'Ruangan sedang dipakai kelas lain\');
+                    
+                    IF 0 < (SELECT COUNT(id) FROM class_schedules
+                            WHERE room_id = NEW.room_id AND 
+                            day_id = NEW.day_id AND (
+                            start_id = NEW.start_id OR
+                            end_id = NEW.end_id))
+                    THEN
+                        SIGNAL SQLSTATE \'45000\'
+                        SET MESSAGE_TEXT = errorMessage;
+                    END IF;
+                          
+                ;END
         ');
     }
 
@@ -50,6 +69,8 @@ return new class extends Migration
      */
     public function down()
     {
-        DB::unprepared('DROP TRIGGER `aaa`');
+        DB::unprepared('
+        DROP TRIGGER `before_insert_schedules`;
+        DROP TRIGGER `before_update_schedules`');
     }
 };
