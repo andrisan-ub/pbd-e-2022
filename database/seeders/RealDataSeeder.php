c065033a7b65ac4fa7361db53e673928852f96bd
Siti Waheeda Najihah <sitiwaheeda14@student.ub.ac.id>
2022-12-06 19:08:59 +0700

    Update RealDataSeeder.php

--- database/seeders/RealDataSeeder.php
+++ database/seeders/RealDataSeeder.php
@@ -15,10 +15,76 @@ class RealDataSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('study_program')->insert([
+        DB::table('faculties')->insert([
+            'id'=>1,
+            'name'=>'Ilmu Komputer'
+        ]);
+
+        DB::table('departments')->insert([
             'id'=>1,
             'faculty_id'=>1,
+            'name'=>'Sistem Informasi'
+        ]);
+
+        DB::table('study_programs')->insert([
+            'id'=>1,
+            'department_id'=>1,
             'name'=>'Teknologi Informasi'
         ]);
+
+        DB::table('courses')->insert([
+            'id'=>1,
+            'study_program_id'=>1,
+            'name'=>'Kewirausahaan'
+        ]);
+
+        DB::table('courses')->insert([
+            'id'=>2,
+            'study_program_id'=>1,
+            'name'=>'Analisis dan Desain Sistem Informasi'
+        ]);
+
+        DB::table('courses')->insert([
+            'id'=>3,
+            'study_program_id'=>1,
+            'name'=>'Jaringan Komputer Dasar'
+        ]);
+
+        DB::table('courses')->insert([
+            'id'=>4,
+            'study_program_id'=>1,
+            'name'=>'Pemrograman Basis Data'
+        ]);
+
+        DB::table('courses')->insert([
+            'id'=>5,
+            'study_program_id'=>1,
+            'name'=>'Algoritma dan Struktur Data'
+        ]);
+
+        DB::table('courses')->insert([
+            'id'=>6,
+            'study_program_id'=>1,
+            'name'=>'Pengembangan Aplikasi Web'
+        ]);
+
+        DB::table('courses')->insert([
+            'id'=>7,
+            'study_program_id'=>1,
+            'name'=>'Etika Profesi'
+        ]);
+
+        DB::table('course_classes')->insert([
+            'id'=>1,
+            'course_id'=>1,
+            'name'=>'A'
+        ]);
+
+        DB::table('course_classes')->insert([
+            'id'=>2,
+            'course_id'=>1,
+            'name'=>'B'
+        ]);
+   
     }
 }
