23ab758701d900c4069ee8c50b1b3c16f90fc230
ABExacta <arenabintang@student.ub.ac.id>
2022-12-06 15:13:39 +0700

    Create table
    Create table survey_pembelajaran

---
+++ database/migrations/2022_12_06_080132_create_survey_pembelajaran_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_pembelajaran', function (Blueprint $table) {
+            $table->id();
+            $table->text('pertanyaan');
+            $table->text('jenis survey');
+            $table->integer('skor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_pembelajaran');
+    }
+};
