4c53e550a125cc611b3928e60024867362163278
Meida Dela Risyafa Auliya <meidadelaris@student.ub.ac.id>
2022-12-06 22:21:19 +0700

    1

---
+++ database/migrations/2022_12_06_123714_trigger_del_post.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+        CREATE TRIGGER BEF_DEL_POST
+        BEFORE DELETE 
+        ON POST
+        FOR EACH ROW 
+            DELETE FROM REPLY
+            WHERE ID_POST = OLD.ID_POST
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER "BEF_DEL_POST"');
+    }
+};
