e8587b4557318fd6b6122bcb2882f2ba2a455583
Wardah Zahra <5wardahzahra@gmail.com>
2022-12-06 21:50:17 +0700

    Update 2022_12_06_131254_create_trigger_insert_schedules.php

--- database/migrations/2022_12_06_131254_create_trigger_insert_schedules.php
+++ database/migrations/2022_12_06_131254_create_trigger_insert_schedules.php
@@ -58,7 +58,7 @@ return new class extends Migration
                         SET MESSAGE_TEXT = errorMessage;
                     END IF;
                           
-                ;END
+                END
         ');
     }
 
