dd4ef1142922c95cdcc649d93bbc0282d26c34fd
Siti Waheeda Najihah <sitiwaheeda14@student.ub.ac.id>
2022-12-06 20:14:29 +0700

    Update RealDataSeeder.php

--- database/seeders/RealDataSeeder.php
+++ database/seeders/RealDataSeeder.php
@@ -35,54 +35,63 @@ class RealDataSeeder extends Seeder
         DB::table('courses')->insert([
             'id'=>1,
             'study_program_id'=>1,
+            'creator_user_id'=>1,
             'name'=>'Kewirausahaan'
         ]);
 
         DB::table('courses')->insert([
             'id'=>2,
             'study_program_id'=>1,
+            'creator_user_id'=>1,
             'name'=>'Analisis dan Desain Sistem Informasi'
         ]);
 
         DB::table('courses')->insert([
             'id'=>3,
             'study_program_id'=>1,
+            'creator_user_id'=>1,
             'name'=>'Jaringan Komputer Dasar'
         ]);
 
         DB::table('courses')->insert([
             'id'=>4,
             'study_program_id'=>1,
+            'creator_user_id'=>1,
             'name'=>'Pemrograman Basis Data'
         ]);
 
         DB::table('courses')->insert([
             'id'=>5,
             'study_program_id'=>1,
+            'creator_user_id'=>1,
             'name'=>'Algoritma dan Struktur Data'
         ]);
 
         DB::table('courses')->insert([
             'id'=>6,
             'study_program_id'=>1,
+            'creator_user_id'=>1,
             'name'=>'Pengembangan Aplikasi Web'
         ]);
 
         DB::table('courses')->insert([
             'id'=>7,
             'study_program_id'=>1,
+            'creator_user_id'=>1,
             'name'=>'Etika Profesi'
         ]);
 
         DB::table('course_classes')->insert([
             'id'=>1,
             'course_id'=>1,
+            'creator_user_id'=>1,
             'name'=>'A'
         ]);
 
         DB::table('course_classes')->insert([
             'id'=>2,
             'course_id'=>1,
+            'creator_user_id'=>1,
             'name'=>'B'
         ]);
    
