643354f721c3ceb53c24928537100a4b514269b3
ghaniatan <ghaniatan@student.ub.ac.id>
2022-12-07 13:10:40 +0700

    Update 2022_12_06_142926_create_survey_history_table.php

--- database/migrations/2022_12_06_142926_create_survey_history_table.php
+++ database/migrations/2022_12_06_142926_create_survey_history_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('survey_history', function (Blueprint $table) {
             $table->id();
-            $table->string('users_name');
+            $table->string('survey_pembelajaran_id');
             $table->text('action');
             $table->timestamp('date');
         });
