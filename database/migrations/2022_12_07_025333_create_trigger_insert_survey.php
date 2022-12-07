990be4ac404bf11509546d0f41db5050329a1495
ghaniatan <ghaniatan@student.ub.ac.id>
2022-12-07 10:26:20 +0700

    Create Trigger insert_survey

---
+++ database/migrations/2022_12_07_025333_create_trigger_insert_survey.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::unprepared
+            ('CREATE TRIGGER `insert_survey` 
+            AFTER INSERT ON `survey_pembelajaran` 
+            FOR EACH ROW INSERT INTO survey_history 
+            VALUES(NULL, NEW.id, `Data inserted`, NOW());');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trigger_insert_survey');
+    }
+};
